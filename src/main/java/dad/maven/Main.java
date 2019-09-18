18abbcb7fc3e02fbbc165f323d7669301be3a4f6
Beatriz Garcia <beagh92@gmail.com>
2019-09-18 11:02:37 +0100

    primer commit

---
+++ src/main/java/dad/maven/Main.java
@@ -0,0 +1,13 @@
+package dad.maven;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class Main {
+
+	public static void main(String[] args) {
+		String invertido = StringUtils.reverse("oilime");
+		System.out.println(invertido);
+
+	}
+
+}
